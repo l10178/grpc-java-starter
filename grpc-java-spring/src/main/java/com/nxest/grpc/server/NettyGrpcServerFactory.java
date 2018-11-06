534a6923b01e996dfbe6c4a97290bd85de4c8ea3
l10178 <l10178@163.com>
2018-11-06 17:29:07 +0800

    bugfix

--- grpc-java-spring/src/main/java/com/nxest/grpc/server/NettyGrpcServerFactory.java
+++ grpc-java-spring/src/main/java/com/nxest/grpc/server/NettyGrpcServerFactory.java
@@ -146,7 +146,7 @@ public class NettyGrpcServerFactory implements GrpcServerFactory {
             String privateKeyFile = security.getPrivateKeyFile();
 
             SslContextBuilder sslClientContextBuilder;
-            if (Strings.isNullOrEmpty(certChainFile) || Strings.isNullOrEmpty(certChainFile)) {
+            if (Strings.isNullOrEmpty(certChainFile) || Strings.isNullOrEmpty(privateKeyFile)) {
                 logger.warning("Grpc server SSL/TLS certChainFile or privateKeyFile  is empty,use default SelfSignedCertificate.");
                 SelfSignedCertificate ssc = new SelfSignedCertificate();
                 sslClientContextBuilder = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey());
